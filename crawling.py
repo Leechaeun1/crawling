c3cc25953c99b49065eafeb522c8ea84f48ad83f
Leechaeun1 <lce001105@gmail.com>
2023-09-18 14:03:27 +0900

    속도

--- crawling.py
+++ crawling.py
@@ -6,16 +6,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import urllib.request
 import os
+import concurrent.futures
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--headless")
 
 driver = webdriver.Chrome(options=chrome_options)
 
 search_terms = ['강아지', '고양이', '얼룩말']
 
-download_path = r'C:\Users\hs9hs\크롤링' 
+download_path = r'C:\Users\hs9hs\크롤링'
 os.makedirs(download_path, exist_ok=True)
+
+def download_image(image_url, file_path, index, search_term):
+    try:
+        urllib.request.urlretrieve(image_url, file_path)
+        print(f"Image {index} for '{search_term}' downloaded to: {file_path}")
+    except Exception as e:
+        print(f"An error occurred while downloading image {index} for '{search_term}': {e}")
 
 for search_term in search_terms:
     URL = f'https://www.google.co.kr/imghp?q={search_term}'
@@ -28,15 +36,15 @@ for search_term in search_terms:
 
     num_images_to_save = 11  # 이미지 저장 개수
 
-    for index, image in enumerate(image_results[:num_images_to_save], start=1):
-        image_url = image.get_attribute('src')
-        file_name = f'{search_term.replace(" ", "_")}_image_{index}.jpg' 
-        file_path = os.path.join(download_path, file_name)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for index, image in enumerate(image_results[:num_images_to_save], start=1):
+            image_url = image.get_attribute('src')
+            file_name = f'{search_term.replace(" ", "_")}_image_{index}.jpg'
+            file_path = os.path.join(download_path, file_name)
+            future = executor.submit(download_image, image_url, file_path, index, search_term)
+            futures.append(future)
 
-        try:
-            urllib.request.urlretrieve(image_url, file_path)
-            print(f"Image {index} for '{search_term}' downloaded to: {file_path}")
-        except Exception as e:
-            print(f"An error occurred while downloading image {index} for '{search_term}': {e}")
+        concurrent.futures.wait(futures)
 
 driver.quit()
